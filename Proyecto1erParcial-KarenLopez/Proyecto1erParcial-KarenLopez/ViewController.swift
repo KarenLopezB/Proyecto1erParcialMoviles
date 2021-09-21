4e8996f6dc023448f9c48bd2d96c5130b2b902a1
Alumno <ulsa@D115.local>
2021-09-20 17:39:56 -0700

    Avances

---
+++ Proyecto1erParcial-KarenLopez/Proyecto1erParcial-KarenLopez/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proyecto1erParcial-KarenLopez
+//
+//  Created by Alumno on 9/20/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
